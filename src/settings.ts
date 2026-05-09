661cf7b7f9f13a1dd4ba2bf1527f1124d0ff6f2f
ste <513049097@qq.com>
2026-05-09 10:08:21 +0800

    Initial plugin

--- src/settings.ts
+++ src/settings.ts
@@ -1,36 +1,27 @@
-import {App, PluginSettingTab, Setting} from "obsidian";
-import MyPlugin from "./main";
+import { App, PluginSettingTab } from "obsidian";
+import NoteTypePlugin from "./main";
 
-export interface MyPluginSettings {
-	mySetting: string;
+export interface NoteTypePluginSettings {
+	propertyIcon: string;
+	keys: string[];
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export const DEFAULT_SETTINGS: NoteTypePluginSettings = {
+	propertyIcon: "",
+	keys: [],
+};
 
 export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: NoteTypePlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: NoteTypePlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
